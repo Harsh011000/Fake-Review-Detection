2167f07fb98df2fe092d0363e6ba23f91213325b
Harsh Panwar <panwarharsh04@gmail.com>
2025-03-19 18:18:14 +0530

    updated api.py

--- APi.py
+++ APi.py
@@ -87,4 +87,4 @@ def predict():
     return jsonify({"prediction": result[0], "score": result[1]})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
