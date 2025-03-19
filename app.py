67cb8dad5901a95db448c2f611d85fa960cb7a46
Harsh Panwar <panwarharsh04@gmail.com>
2025-03-20 01:10:49 +0530

    ui

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+st.title("📝 Fake Review Detection")
+st.write("Enter a review below to check whether it's Fake or Real.")
+
+user_input = st.text_area("Enter a review:")
+
+if st.button("Analyze Review"):
+    if user_input:
+        response = requests.post("https://harsh-me-fake-review-detection.hf.space/predict", json={"review": user_input})
+        result = response.json()
+        st.subheader(result["prediction"])
+        st.write(f"Confidence Score: {result['score']:.4f}")
+    else:
+        st.warning("Please enter a review.")
